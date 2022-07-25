c264fca9801ddae095ff210396424f53f42f37c4
Matthew <matt@souldo.net>
2022-07-25 14:22:44 -0500

    Initial Commit

---
+++ plans/deploy_nexus_choco_server.pp
@@ -0,0 +1,36 @@
+plan chocodemo::deploy_nexus_choco_server(
+  TargetSpec $targets,
+  String $repo_name
+) {
+  apply($targets, _description => 'Installing Nexus OSS package via Chocolatey') {
+    package { 'nexus-repository' :
+      ensure   => present,
+      provider => chocolatey,
+    }
+  }
+
+  $password = run_command(
+    '(Get-Content C:\ProgramData\sonatype-work\nexus3\admin.password).ToString()',
+    _description => 'Retreiving temporary password...'
+  )
+
+  run_command(
+    'netsh advfirewall firewall add rule name="Nexus" dir=in action=allow protocol=TCP localport=8081',
+    _description => 'Configuring firewall settings...',
+    $targets
+  )
+
+  apply($targets) {
+    package { 'chocolatey-nexus-repo':
+      ensure          => present,
+      provider        => chocolatey,
+      install_options => [
+        '-params',
+        '"',
+        '/username=admin',
+        "/password=${password}",
+        "/repositoryname=${repo_name}",
+        '"',
+      ],
+  }
+}
