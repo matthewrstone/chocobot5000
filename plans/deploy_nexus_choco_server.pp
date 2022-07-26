a63cffc9e09b356fc71fe22cf74ea16ea9175735
Matthew <matt@souldo.net>
2022-07-26 09:53:18 -0500

    remove apply command

--- plans/deploy_nexus_choco_server.pp
+++ plans/deploy_nexus_choco_server.pp
@@ -2,36 +2,37 @@ plan chocobot5000::deploy_nexus_choco_server (
   TargetSpec $targets,
   String $repo_name,
 ) {
-  apply($targets, _description => 'Installing Nexus OSS package via Chocolatey') {
-    package { 'nexus-repository' :
-      ensure   => present,
-      provider => chocolatey,
-    }
-  }
+  # apply($targets, _description => 'Installing Nexus OSS package via Chocolatey') {
+  #   package { 'nexus-repository' :
+  #     ensure   => present,
+  #     provider => chocolatey,
+  #   }
+  # }
+  run_command('choco install nexus-repository -y', $targets)
 
   $password = run_command(
     '(Get-Content C:\ProgramData\sonatype-work\nexus3\admin.password).ToString()',
     _description => 'Retreiving temporary password...'
   )
 
-  run_command(
-    'netsh advfirewall firewall add rule name="Nexus" dir=in action=allow protocol=TCP localport=8081',
-    _description => 'Configuring firewall settings...',
-    $targets
-  )
+  return $password
+  # run_command(
+  #   'netsh advfirewall firewall add rule name="Nexus" dir=in action=allow protocol=TCP localport=8081',
+  #   _description => 'Configuring firewall settings...',
+  #   $targets
+  # )
 
-  apply($targets) {
-    package { 'chocolatey-nexus-repo':
-      ensure          => present,
-      provider        => chocolatey,
-      install_options => [
-        '-params',
-        '"',
-        '/username=admin',
-        "/password=${password}",
-        "/repositoryname=${repo_name}",
-        '"',
-      ],
-    }
-  }
+  # apply($targets) {
+  #   package { 'chocolatey-nexus-repo':
+  #     ensure          => present,
+  #     provider        => chocolatey,
+  #     install_options => [
+  #       '-params',
+  #       '"',
+  #       '/username=admin',
+  #       "/password=${password}",
+  #       "/repositoryname=${repo_name}",
+  #       '"',
+  #     ],
+  #   }
 }
