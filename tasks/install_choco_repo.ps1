081459e0bd151747c39451f3fa84f00351dc1d70
Matthew <matt@souldo.net>
2022-07-26 16:55:58 -0500

    adsf

--- tasks/install_choco_repo.ps1
+++ tasks/install_choco_repo.ps1
@@ -1,9 +1,10 @@
 #!/usr/local/bin/pwsh
 [CmdletBinding()]
 Param(
+  $username='admin',
   $server,
   $password
 )
 
 Write-Output $server $password
-choco install chocolatey-nexus-repo -y --params=\"'/Username=admin /Password=${password} /RepositoryName=choco /ServerUri=http://${server}:8081'\"
+choco install chocolatey-nexus-repo -y --params="'/Username=admin /Password=${password} /RepositoryName=choco /ServerUri=http://${server}:8081'"
